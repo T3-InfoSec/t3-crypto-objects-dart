2d3233e38d7e22d0b67cc2a6faea1d746dd46943
flatFeather <github.astronaut143@silomails.com>
2024-12-08 23:41:39 +0100

    Refactor formosa interface

--- lib/src/entropy/formosa/formosa.dart
+++ lib/src/entropy/formosa/formosa.dart
@@ -1,22 +1,16 @@
-import 'dart:typed_data';
-
 import 'package:t3_crypto_objects/crypto_objects.dart';
 
 abstract class FormosaEntropy extends Entropy {
   dynamic formosaTheme;
-  late String mnemonic;
 
   /// Creates an instance of Formosa using the given formosaTheme and initial entropy.
-  ///
-  /// The [mnemonic] is automatically derived based on the initial entropy [value] provided
-  /// and the words defined by the specified FormosaTheme.
-  FormosaEntropy(super.value, this.formosaTheme) {
-    mnemonic = _toMnemonic(value);
-  }
+  FormosaEntropy(super.value, this.formosaTheme);
 
   factory FormosaEntropy.fromMnemonic(String mnemonic, dynamic formosaTheme) {
     throw UnimplementedError('fromMnemonic must be implemented in subclasses');
   }
   
-  String _toMnemonic(Uint8List entropy);
+  /// Return derived mnemonic based on the entropy [value]
+  /// and the words defined by the specified FormosaTheme.
+  String getMnemonic();
 }
