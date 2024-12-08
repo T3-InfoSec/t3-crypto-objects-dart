7f7a4a6d10e5585060aea67deac5b4df82af6179
flatFeather <github.astronaut143@silomails.com>
2024-12-09 00:10:23 +0100

    Add multiple of 4 validation for FormosaEntropy

--- lib/src/entropy/formosa/formosa.dart
+++ lib/src/entropy/formosa/formosa.dart
@@ -1,10 +1,17 @@
+import 'dart:typed_data';
+
 import 'package:t3_crypto_objects/crypto_objects.dart';
 
 abstract class FormosaEntropy extends Entropy {
+  static final leastMultiple = 4;
   dynamic formosaTheme;
 
   /// Creates an instance of Formosa using the given formosaTheme and initial entropy.
-  FormosaEntropy(super.value, this.formosaTheme);
+  FormosaEntropy(Uint8List value, this.formosaTheme) : super(value) {
+    if (value.length % leastMultiple != 0) {
+      throw ArgumentError('Formosa entropy length must be multiple of $leastMultiple');
+    }
+  }
 
   factory FormosaEntropy.fromMnemonic(String mnemonic, dynamic formosaTheme) {
     throw UnimplementedError('fromMnemonic must be implemented in subclasses');
