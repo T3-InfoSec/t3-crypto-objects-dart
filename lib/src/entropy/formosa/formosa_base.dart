862e9b445e85e5a8bd7aab00cf7c26ad419cc72b
flatFeather <github.astronaut143@silomails.com>
2024-12-11 00:13:04 +0100

    Add documentation comments

--- lib/src/entropy/formosa/formosa_base.dart
+++ lib/src/entropy/formosa/formosa_base.dart
@@ -8,8 +8,14 @@ import 'package:t3_crypto_objects/src/entropy/entropy_bytes.dart';
 
 import 'formosa_theme.dart';
 
-/// A wrap implementation upon bip39 for supporting semantically connected
-/// words implementation upon multiple supported themes.
+/// A class that implements BIP39 with support for semantically connected word 
+/// implementations across multiple themes.
+///
+/// This class encapsulates entropy bytes and provides methods to generate a
+/// mnemonic, validate checksums, and convert between bits and entropy. It supports
+/// different themes for the word list and checksum mechanisms. This class is 
+/// designed to ensure that entropy lengths are valid and that the formosa 
+/// instance is correctly validated after creation.
 class Formosa extends EntropyBytes {
   static const leastMultiple = 4;
   static var invalidFormosa = Formosa(Uint8List(0), FormosaTheme.bip39);
@@ -25,19 +31,56 @@ class Formosa extends EntropyBytes {
     }
   }
 
-  /// Constructor that  creates a formosa instance from [concatenatedBits].
+  /// Creates a [Formosa] instance from a concatenated list of bits.
+  ///
+  /// This constructor takes a list of boolean values representing concatenated 
+  /// bits (including both entropy and checksum bits) and constructs a [Formosa] 
+  /// object. The method ensures that the checksum and entropy are correctly 
+  /// parsed from the [concatenatedBits].
   factory Formosa.fromConcatenatedBits(
     List<bool> concatenatedBits, {
     required FormosaTheme formosaTheme,
   }) {
+    return _generateFormosaFromContatenatedBits(concatenatedBits, formosaTheme);
+  }
+
+  /// Creates a [Formosa] instance from a mnemonic string.
+  ///
+  /// This constructor takes a [mnemonic] (a space-separated list of words) and 
+  /// converts it into a [Formosa] instance. The mnemonic is parsed into bits, 
+  /// with the entropy and checksum being separated and validated. The mnemonic 
+  /// must conform to the expected word count and structure for the chosen theme.
+  factory Formosa.fromMnemonic(
+    String mnemonic, {
+    FormosaTheme formosaTheme = FormosaTheme.bip39,
+  }) {
+    return _generateFormosaFromMnemonic(mnemonic, formosaTheme);
+  }
+
+  /// Returns the mnemonic phrase corresponding to the current entropy value.
+  ///
+  /// This method converts the current entropy bytes into a mnemonic string 
+  /// based on the theme's word list.
+  String getMnemonic() => _entropyToMnemonic(value);
+
+  /// Validates the current instance by checking if the checksum matches the 
+  /// entropy value.
+  ///
+  /// This method verifies that the checksum of the entropy bytes matches the 
+  /// expected checksum derived from the entropy value. It returns true if the 
+  /// checksum is valid, and false otherwise.
+  bool isValid() => _validateChecksum(value, checksumBits);
+  
+
+  static Formosa _generateFormosaFromContatenatedBits(List<bool> concatenatedBits, FormosaTheme formosaTheme) {
     final totalBits = concatenatedBits.length;
     final checksumLength = totalBits ~/ 33;
-
+    
     final entropyBits = concatenatedBits.take(totalBits - checksumLength).toList();
     final checksumBits = concatenatedBits.skip(totalBits - checksumLength).toList();
-
+    
     final entropyBytes = EntropyBits.boolBitsToBytes(entropyBits);
-
+    
     return Formosa(
       entropyBytes,
       formosaTheme,
@@ -45,55 +88,47 @@ class Formosa extends EntropyBytes {
     );
   }
 
-  /// Constructor that  creates a formosa instance from [mnemonic].
-  factory Formosa.fromMnemonic(
-    String mnemonic, {
-    FormosaTheme formosaTheme = FormosaTheme.bip39,
-  }) {
-    List<String> words = mnemonic.split(' ');
+  static Formosa _generateFormosaFromMnemonic(String mnemonic, FormosaTheme formosaTheme) {
+       List<String> words = mnemonic.split(' ');
     var wordsDict = formosaTheme.data;
-
+    
     int phraseSize = wordsDict.wordsPerPhrase();
-
+    
     // Check if the number of words is a multiple of the expected size per phrase
     if (words.length % phraseSize != 0) {
       return invalidFormosa;
     }
-
+    
     // Calculate the length of the concatenated bits, and determine the checksum and entropy lengths
     int concatenationLenBits = words.length * wordsDict.bitsPerPhrase();
     int checksumLengthBits = concatenationLenBits ~/ 33;
     int entropyLengthBits = concatenationLenBits - checksumLengthBits;
-
+    
     // Get the phrase indexes based on the word list
     List<int> phraseIndexes = wordsDict.getPhraseIndexes(words);
-
+    
     // Convert phrase indexes to a concatenated bit string
     String concatenationBits = phraseIndexes
         .map((index) => index.toRadixString(2).padLeft(11, '0'))
         .join();
-
+    
     // Extract the entropy bits from the concatenation (excluding the checksum bits)
     String entropyBits = concatenationBits.substring(0, entropyLengthBits);
-
+    
     // Convert the string of bits to a Uint8List entropy bytes.
     Uint8List entropy = EntropyBits.stringBitsToBytes(entropyBits);
-
+    
     // Create the ChecksumBits from the corresponding bits of the concatenation
     List<bool> bits = concatenationBits
         .substring(entropyLengthBits)  // Get the checksum part
         .split('')
         .map((bit) => bit == '1')
         .toList();
-
+    
     ChecksumBits checksumBits = ChecksumBits(bits);
-
+    
     return Formosa(entropy, formosaTheme, checksumBits: checksumBits);
   }
-
-  String getMnemonic() => _entropyToMnemonic(value);
-
-  bool isValid() => _validateChecksum(value, checksumBits);
 
   static ChecksumBits _generateChecksumBits(Uint8List value) {
     final hash = sha256.convert(value);
