a52cf8dd6d762b2136e8b68398a7fa979063c67e
flatFeather <github.astronaut143@silomails.com>
2025-01-13 21:29:31 +0100

    Update KA and tests

--- lib/src/entropy/ka.dart
+++ lib/src/entropy/ka.dart
@@ -1,5 +1,8 @@
 import 'package:convert/convert.dart';
+import 'package:t3_crypto_objects/src/argon2/argon2_derivation_service.dart';
+import 'package:t3_crypto_objects/src/entropy/entropy_bytes.dart';
 import 'package:t3_crypto_objects/src/entropy/formosa/formosa.dart';
+import 'package:t3_crypto_objects/src/entropy/formosa/formosa_theme.dart';
 import 'package:t3_crypto_objects/src/entropy/node.dart';
 
 /// The final node in the tacit derivation process, derived from the PA0 seed.
@@ -10,10 +13,15 @@ class KA extends Node {
   final Formosa formosa;
   final String hexadecimalValue;
 
-  /// Initializes the [KA] node instance with a [formosa] entropy.
+  /// Initializes the [KA] node instance with an entropy [value].
   ///
-  /// Automatically encodes [hexadecimalValue] from formosa entropy [value] during construction.
-  KA(this.formosa)
-      : hexadecimalValue = hex.encode(formosa.value),
-        super(formosa.value);
+  /// Automatically encodes [hexadecimalValue] from entropy [value]
+  /// and generate Formosa bip39 during construction.
+  KA(super.value)
+      : hexadecimalValue = hex.encode(value),
+        formosa = Formosa(
+            Argon2DerivationService()
+                .deriveWithLowMemory(EntropyBytes(value))
+                .value,
+            FormosaTheme.bip39);
 }
