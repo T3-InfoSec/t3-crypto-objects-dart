b6b2f6014ae90432571ca51958c081d80437baaf
flatFeather <github.astronaut143@silomails.com>
2025-01-13 21:29:31 +0100

    Update KA and tests

--- test/src/entropy/ka_test.dart
+++ test/src/entropy/ka_test.dart
@@ -1,3 +1,6 @@
+import 'package:t3_crypto_objects/src/argon2/argon2_derivation_service.dart';
+import 'package:t3_crypto_objects/src/entropy/entropy_bytes.dart';
+import 'package:t3_crypto_objects/src/entropy/formosa/formosa_theme.dart';
 import 'package:t3_crypto_objects/src/entropy/ka.dart';
 import 'package:test/test.dart';
 import 'package:convert/convert.dart';
@@ -5,22 +8,18 @@ import 'package:t3_crypto_objects/src/entropy/formosa/formosa.dart';
 
 void main() {
   group('KA', () {
-    test('KA initializes correctly with valid Formosa', () {
-      final formosa = Formosa.fromRandomWords();
-      final ka = KA(formosa);
+    test('KA initializes correctly with valid hexadecimal Value Formosa', () {
+      final entropy = EntropyBytes.fromRandom(1024);
+      final ka = KA(entropy.value);
 
-      expect(ka.hexadecimalValue, equals(hex.encode(formosa.value)));
-      expect(ka.formosa, equals(formosa));
-      expect(ka.value, equals(formosa.value));
-    });
+      final expectedFormosa = Formosa(
+            Argon2DerivationService()
+                .deriveWithLowMemory(EntropyBytes(entropy.value))
+                .value, FormosaTheme.bip39);
 
-
-    test('KA generates correct hexadecimal value', () {
-      final formosa = Formosa.fromRandomWords();
-      final ka = KA(formosa);
-
-      expect(ka.hexadecimalValue, equals(hex.encode(formosa.value)));
-      expect(ka.formosa, equals(formosa));
+      expect(ka.hexadecimalValue, equals(hex.encode(entropy.value)));
+      expect(ka.formosa, equals(expectedFormosa));
+      expect(ka.value, equals(entropy.value));
     });
   });
 }
